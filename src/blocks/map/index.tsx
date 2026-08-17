80fd181646864aacd08553bbfb1421015443bb97
amohamed <abdulmalikmohamed813@gmail.com>
2026-08-17 02:52:16 -0300

    Update

--- src/blocks/map/index.tsx
+++ src/blocks/map/index.tsx
@@ -24,7 +24,7 @@ export function MapBlockUI({
       />
 
       {text && (
-        <div className="absolute bottom-4 left-4 bg-black px-5 py-3 font-bold uppercase shadow-lg">
+        <div className="absolute bottom-4 left-4 bg-background px-5 py-3 font-bold uppercase shadow-lg">
           {text}
         </div>
       )}
