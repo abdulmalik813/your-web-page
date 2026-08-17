8e5cfc057bcc2eb236df465e32bb08800fc9cd6b
amohamed <abdulmalikmohamed813@gmail.com>
2026-08-16 23:29:14 -0300

    Update

--- src/components/theme/theme-toggle.tsx
+++ src/components/theme/theme-toggle.tsx
@@ -31,7 +31,7 @@ export function ThemeToggle() {
   }
 
   return (
-    <Button variant="outline" size="icon-lg" onClick={cycleTheme} className="w-10 h-10">
+    <Button variant="outline" size="icon-lg" onClick={cycleTheme} className="w-12 h-12">
       {theme === 'system' && <Monitor className="h-[1.2rem] w-[1.2rem] transition-all" />}
       {theme === 'dark' && <Moon className="h-[1.2rem] w-[1.2rem] transition-all" />}
       {theme === 'light' && <Sun className="h-[1.2rem] w-[1.2rem] transition-all" />}
